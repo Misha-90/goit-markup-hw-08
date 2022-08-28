2e618db94d1ecf8424b8065a67894108a0c00d9b
Misha-90 <sharafan911@gmail.com>
2022-08-28 23:38:31 +0300

    mob menu

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector(".mob__menu-btn-open"),
+		closeMenuBtn: document.querySelector(".mob__menu-btn-close"),
+		menu: document.querySelector(".mob__menu"),
+		body: document.querySelector("body"),
+		menuList: document.querySelector(".mob-menu-list"),
+	};
+
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+	refs.menuList.addEventListener("click", removeMenu);
+
+	function toggleMenu() {
+		refs.menu.classList.toggle("is-hidden");
+		refs.body.classList.toggle("no-scroll");
+	}
+
+	function removeMenu() {
+		refs.menu.classList.add("is-hidden");
+	}
+})();
